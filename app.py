1e234789b94a9bc044dc2c683c12c97e8bf97ab4
Achal Vats <achalvats15@gmail.com>
2023-12-22 22:24:32 -0500

    Add Flask-SQLAlchemy and configure database connection

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo_db.db'
+db = SQLAlchemy(app)
+
+# class Todo(db.Model):
+#     id = db.Column(db.I)
 
 @app.route('/')
 def index():
